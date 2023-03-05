17f34274b848a8ef9fd847d9359f2bca809664e2
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-03-05 08:19:15 +0530

    Update readme

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <Welcome name = "Aniket" />;
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  element
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
